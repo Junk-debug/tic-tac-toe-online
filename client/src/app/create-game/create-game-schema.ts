2dbe3b46761d15ab21a782b168826c55add59559
Junk-debug <maks.naumovich@gmail.com>
2024-08-17 18:22:17 +0200

    refactor: create fieldSize enum

--- client/src/app/create-game/create-game-schema.ts
+++ client/src/app/create-game/create-game-schema.ts
@@ -1,8 +1,14 @@
 import { z } from "zod";
 
+export enum FieldSize {
+  "3x3" = "3x3",
+  "5x5" = "5x5",
+  "9x9" = "9x9",
+}
+
 export const createGameFormSchema = z.object({
   playerName: z.string().min(2).max(50),
-  fieldSize: z.enum(["3x3", "5x5", "9x9"]),
+  fieldSize: z.nativeEnum(FieldSize),
 });
 
 export type CreateGameFormSchema = z.infer<typeof createGameFormSchema>;
